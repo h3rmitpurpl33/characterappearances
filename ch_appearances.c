f963eadc442f0609b9088a1844b26e49e981fe04
h3rmit <76179581+h3rmitpurpl33@users.noreply.github.com>
2021-08-30 00:22:39 +0300

    Update ch_appearances.c

--- ch_appearances.c
+++ ch_appearances.c
@@ -1,47 +1,58 @@
-int main(){
-char ch , max_ch,str[100];
-int i , len , max_times , low_let[26] = {0};
-int dig[10] = {0};
-printf("enter text : ");
-len = read_text(str,sizeof(str),1);
-max_ch = max_times = 0;
-for(i=0;i<len;i++)
-{
-    if((str[i]>='a') && (str[i]<='z'))
-    {
-        ch = str[i] - 'a';
-        low_let[ch]++;
+int read_text(char str[], int size, int flag);
 
-        if(low_let[ch]>max_times)
-        {
-            max_times = low_let[ch];
-            max_ch = str[i];
+int main() {
+    char ch, max_ch, str[100];
+    int i, len, max_times, low_let[26] = {0};
+    int dig[10] = {0};
+    printf("enter text : ");
+    len = read_text(str, sizeof(str), 1);
+    max_ch = max_times = 0;
+    for (i = 0; i < len; i++) {
+        if ((str[i] >= 'a') && (str[i] <= 'z')) {
+            ch = str[i] - 'a';
+            low_let[ch]++;
+
+            if (low_let[ch] > max_times) {
+                max_times = low_let[ch];
+                max_ch = str[i];
+            }
+        } else if ((str[i] >= '0') && (str[i] <= '9')) {
+            dig[str[i] - '0']++;
         }
-    }else if((str[i]>='0') && (str[i]<='9'))
-    {
-        dig[str[i]-'0']++;
     }
-}
-printf("*****lower case letters appearances\n");
-for(i=0;i<26;i++)
-{
-    if(low_let[i] != 0)
-    {
-        printf("letter %c appeared %d times\n",'a'+i,low_let[i]);
+    printf("*****lower case letters appearances\n");
+    for (i = 0; i < 26; i++) {
+        if (low_let[i] != 0) {
+            printf("letter %c appeared %d times\n", 'a' + i, low_let[i]);
+        }
     }
-}
-printf("*****digit appearances\n");
-for(i=0;i<10;i++)
-{
-    if(dig[i] != 0)
-    {
-        printf("digit %d appeared %d times\n",i , dig[i]);
+    printf("*****digit appearances\n");
+    for (i = 0; i < 10; i++) {
+        if (dig[i] != 0) {
+            printf("digit %d appeared %d times\n", i, dig[i]);
+        }
     }
-}
-if(max_times != 0)
-{
-    printf("'%c'   appears    %d   times\n", max_ch,max_times);
-}
-  return 0;
+    if (max_times != 0) {
+        printf("'%c'   appears    %d   times\n", max_ch, max_times);
+    }
+    return 0;
 }
 
+int read_text(char string[], int size, int flag) {
+    int len;
+    if (fgets(string, size, stdin) == NULL) {
+        printf("error : fgets() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    len = strlen(string);
+    if (len > 0) {
+        if (flag && (string[len - 1] == '\n')) {
+            string[len - 1] = '\0';
+            len--;
+        }
+    } else {
+        printf("error : no input\n");
+        exit(EXIT_FAILURE);
+    }
+    return len;
+}
